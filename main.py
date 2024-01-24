19cbf07d17c98a4582604b86bcca65cf8fc1d434
yanghao18 <yanghao18@lenovo.com>
2024-01-24 15:12:21 +0800

    log Tokens

--- main.py
+++ main.py
@@ -2,20 +2,27 @@ import os
 from pathlib import Path
 from llama_index.llms import OpenAI
 from llama_index import download_loader
-from llama_hub.github_repo import  GithubClient
+from llama_hub.github_repo import GithubClient
 from llama_index.tools import QueryEngineTool, ToolMetadata
 from llama_index.node_parser import SentenceSplitter
-from llama_index import  ServiceContext, VectorStoreIndex
+from llama_index import ServiceContext, VectorStoreIndex, set_global_service_context,global_service_context
 from llama_index.agent import OpenAIAgent
 from llama_index import SimpleDirectoryReader
+from llama_index.callbacks import CallbackManager, TokenCountingHandler
+import tiktoken
 
-
-llm = OpenAI(temperature=0, model="gpt-3.5-turbo")
+model = "gpt-3.5-turbo"
+llm = OpenAI(temperature=0, model=model)
+token_counter = TokenCountingHandler(
+    tokenizer=tiktoken.encoding_for_model(model).encode
+)
+callback_manager = CallbackManager([token_counter])
+service_context = ServiceContext.from_defaults(llm=llm)
+node_parser = SentenceSplitter()
+set_global_service_context(service_context)
 
 
 def buildGithubEngineTool() -> QueryEngineTool:
-    service_context = ServiceContext.from_defaults(llm=llm)
-    node_parser = SentenceSplitter()
     GithubRepositoryReader = download_loader("GithubRepositoryReader")
     github_client = GithubClient(os.getenv("GITHUB_TOKEN"))
     loader = GithubRepositoryReader(
@@ -34,8 +41,7 @@ def buildGithubEngineTool() -> QueryEngineTool:
     docs = loader.load_data(branch="main")
     nodes = node_parser.get_nodes_from_documents(docs)
     print("code nodes size -> "+str(len(nodes)))
-    vector_index = VectorStoreIndex(
-        nodes=nodes, service_context=service_context)
+    vector_index = VectorStoreIndex(nodes=nodes,service_context=global_service_context)
     vector_index.storage_context.persist('./cache/github')
     vector_query_engine = vector_index .as_query_engine()
 
@@ -44,15 +50,13 @@ def buildGithubEngineTool() -> QueryEngineTool:
         metadata=ToolMetadata(
             name="code_vector_tool",
             description=(
-                "Vector Index for querying source code of java system"
+                "Vector Index for querying source code of java system. When you want to locate the error code you can read all the code here."
             ),
         ),
     )
 
 
 def buildLogEngineTool() -> QueryEngineTool:
-    service_context = ServiceContext.from_defaults(llm=llm)
-    node_parser = SentenceSplitter()
     UnstructuredReader = download_loader('UnstructuredReader')
     dir_reader = SimpleDirectoryReader('./data/log', file_extractor={
         ".log": UnstructuredReader(), })
@@ -60,8 +64,7 @@ def buildLogEngineTool() -> QueryEngineTool:
 
     nodes = node_parser.get_nodes_from_documents(documents)
     print("log nodes size -> "+str(len(nodes)))
-    vector_index = VectorStoreIndex(
-        nodes=nodes, service_context=service_context)
+    vector_index = VectorStoreIndex(nodes=nodes,service_context=global_service_context)
     vector_index.storage_context.persist('./cache/logs')
     vector_query_engine = vector_index .as_query_engine()
 
@@ -70,30 +73,46 @@ def buildLogEngineTool() -> QueryEngineTool:
         metadata=ToolMetadata(
             name="log_vector_tool",
             description=(
-                "Vector Index for querying log messages of java system"
+                "Vector Index for querying log messages of java system. You can use this tool to look up error messages when you need to."
             ),
         ),
     )
 
 
-function_llm = OpenAI()
-agent = OpenAIAgent.from_tools(
-    [buildGithubEngineTool(),
-     buildLogEngineTool(),],
-    llm=function_llm,
-    verbose=True,
-    system_prompt=f"""\
-    You are an automated ops bot that helps developers query the specific code that is having problems and provide solutions.
-    You need to query log with log_vector_tool and then find the reated code with code_vector_tool.
-    You must ALWAYS use tools provided when answering a question; do NOT rely on prior knowledge.\
-    """,
-)
+def init_agent() -> OpenAIAgent:
+    function_llm = OpenAI(callback_manager=callback_manager)
+    return OpenAIAgent.from_tools(
+        [buildGithubEngineTool(),
+         buildLogEngineTool(),],
+        llm=function_llm,
+        verbose=True,
+        system_prompt=f"""\
+        You are an automated ops bot that helps developers query the specific code that is having problems and provide solutions.
+        You need to query logs with log_vector_tool and then find the reated code with code_vector_tool.
+        You must ALWAYS use tools provided when answering a question; do NOT rely on prior knowledge.\
+        """,
+    )
 
-while 1:
-    question = input('Input Question：')
-    if question == 'exit':
-        break
-    response = agent.query(question)
-    print(response)
 
-# Which line of code caused the error reported in the log?
+if __name__ == "__main__":
+    agent = init_agent()
+    while 1:
+        question = input('Input Question：')
+        if question == 'exit':
+            break
+        response = agent.query(question)
+        print(response)
+        print(
+            "Embedding Tokens: ",
+            token_counter.total_embedding_token_count,
+            "\n",
+            "LLM Prompt Tokens: ",
+            token_counter.prompt_llm_token_count,
+            "\n",
+            "LLM Completion Tokens: ",
+            token_counter.completion_llm_token_count,
+            "\n",
+            "Total LLM Token Count: ",
+            token_counter.total_llm_token_count,
+            "\n",
+        )
